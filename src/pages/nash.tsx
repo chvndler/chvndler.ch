8834b3200c2974f9433df8247979f125214d0efa
chvndler <chan@ady.world>
2022-08-20 13:17:08 -0400

    nash

---
+++ src/pages/nash.tsx
@@ -0,0 +1,171 @@
+import { Box, Container, Paragraph, Section, Text } from '@/ui';
+import { ColorBar } from '@/components/ColorBar';
+import { NavBar } from '@/components/NavBar';
+import { ChvndlerCh } from '@/components/logo/ChvndlerCh';
+
+
+const Nash = () => {
+  return (
+    <>
+      <Box css={{ backgroundColor: '$mauve1' }}>
+        <NavBar />
+        <ColorBar />
+        <Section size='3'>
+          <Container size='2' css={{ paddingTop: '40px', paddingBottom: '40px' }}>
+            <Text
+              css={{ fontSize: '14px', fontWeight: 'bold', paddingBottom: '40px', fontFamily: '$pragmaticaExtended' }}>
+              NASHELLY MORONTA®
+            </Text>
+            <Paragraph size='1' css={{ lineHeight: 'normal', fontFamily: '$pragmaticaExtended', fontWeight: 'normal' }}>
+              today feels a little different i think.<br />
+              i’m not entirely sure why, but different enough to notice it.
+            </Paragraph>
+            <br /><br />
+            <Paragraph size='1' css={{ lineHeight: 'normal', fontFamily: '$pragmaticaExtended', fontWeight: 'normal' }}>
+              i’ve said so many words to you since the day i met you. we’ve talked about a lot. we’ve felt a lot. but i
+              want the words you are about to read, to mean the most to you.
+            </Paragraph>
+            <br /><br />
+            <Paragraph size='1' css={{ lineHeight: 'normal', fontFamily: '$pragmaticaExtended', fontWeight: 'normal' }}>
+              i hope you to see them, hear them, feel them.
+              trust them. <br />and i never want you to lose them..
+            </Paragraph>
+
+            <br /><br />
+            <Paragraph size='1' css={{ lineHeight: 'normal', fontFamily: '$pragmaticaExtended', fontWeight: 'normal' }}>
+              to be honest nash.. <br />
+              i've never felt something so engaging in my entire life.
+            </Paragraph>
+            <br /><br />
+            <Paragraph size='1' css={{ lineHeight: 'normal', fontFamily: '$pragmaticaExtended', fontWeight: 'normal' }}>
+              when we first started talking.
+              it was mad exciting.
+              it was exciting because i could feel it coming.
+              i began noticing how you spoke to me.
+              and when you chose to speak to me.
+              it was different.
+              our energy was rare, and i think you felt it too.
+            </Paragraph>
+
+            <br /><br />
+            <Paragraph size='1' css={{ lineHeight: 'normal', fontFamily: '$pragmaticaExtended', fontWeight: 'normal' }}>
+              Im not someone to fall in love quick. to love quick... yes. I love things about a person quick. and I
+              eventually love enough things about a person to say, "I love you".
+
+              with previous relationships, i always noticed that, if i was being honest with chvn®, I knew it wasnt it,
+              they didnt inspire me, push me, or even make me feel.
+            </Paragraph>
+
+            <br /><br />
+            <Paragraph size='1' css={{ lineHeight: 'normal', fontFamily: '$pragmaticaExtended', fontWeight: 'normal' }}>
+              Ever since the day i knew i loved you. Since the day i gave you the first kiss. My only goal was
+              to be "More" with you..
+            </Paragraph>
+
+            <br /><br />
+            <Paragraph size='1' css={{ lineHeight: 'normal', fontFamily: '$pragmaticaExtended', fontWeight: 'normal' }}>
+              I know what we've been through has been hard on you, your family, your friends, and your heart. and i know
+              sometimes you may feel like you shouldnt have done it.
+              maybe small regrets..
+            </Paragraph>
+
+            <br /><br />
+            <Paragraph size='1' css={{ lineHeight: 'normal', fontFamily: '$pragmaticaExtended', fontWeight: 'normal' }}>
+              But for me, it was something I had to do.
+            </Paragraph>
+
+            <br /><br />
+            <Paragraph size='1' css={{ lineHeight: 'normal', fontFamily: '$pragmaticaExtended', fontWeight: 'normal' }}>
+              I know you are still healing. and im sure yesterday was a little difficult.
+              therefore, I never want to lay any pressure on you. But I want you to know...
+            </Paragraph>
+
+            <br /><br />
+            <Paragraph size='1' css={{ lineHeight: 'normal', fontFamily: '$pragmaticaExtended', fontWeight: 'normal' }}>
+              I love everything about you nash. I love us and our ways. I see no need for anyone else.
+            </Paragraph>
+
+            <br /><br />
+            <Paragraph size='1' css={{ lineHeight: 'normal', fontFamily: '$pragmaticaExtended', fontWeight: 'normal' }}>
+              We've never had a label. and I know thats not something either of us are looking for. we can take it slow,
+              or we can go faster.
+              it does not matter. but I LOVE YOU, and i swear that is true.
+            </Paragraph>
+
+            <br /><br />
+            <Paragraph size='1' css={{ lineHeight: 'normal', fontFamily: '$pragmaticaExtended', fontWeight: 'normal' }}>
+              if you love me, lets do this thing. lets be more.
+            </Paragraph>
+
+            <br /><br />
+            <Paragraph size='1' css={{ lineHeight: 'normal', fontFamily: '$pragmaticaExtended', fontWeight: 'normal' }}>
+              below are my promises to you nash.
+            </Paragraph>
+            <br /><br />
+            <Text
+              css={{ fontSize: '14px', fontWeight: 'bold', paddingBottom: '40px', fontFamily: '$pragmaticaExtended' }}>
+              MY PROMISES©
+            </Text>
+            <br /><br />
+            <Paragraph size='1' css={{ lineHeight: 'normal', fontFamily: '$pragmaticaExtended', fontWeight: 'normal' }}>
+              <ul>
+                <li>to always be myself.</li>
+                <li>to always tell the truth.</li>
+                <li>to protect you.</li>
+                <li>to listen to you.</li>
+                <li>to inspire you.</li>
+                <li>to trust you.</li>
+                <li>to help you.</li>
+                <li>to respect you.</li>
+                <li>to sacrifice for you.</li>
+                <li>to support you and your dreams.</li>
+                <li>to be loyal and to always be there.</li>
+                <li>to be your best friend.</li>
+                <li>to love you unconditionally.</li>
+                <li>lastly, i always promised to you, that you would be my girl one day.</li>
+              </ul>
+            </Paragraph>
+
+            <br /><br />
+            <Text
+              css={{ fontSize: '14px', fontWeight: 'bold', paddingBottom: '40px', fontFamily: '$pragmaticaExtended' }}>
+              Chicas, <br />will you be more with me?
+            </Text>
+            <br /><br />
+            <Paragraph size='1' css={{ lineHeight: 'normal', fontFamily: '$pragmaticaExtended', fontWeight: 'normal' }}>
+              lets set goals together.
+            </Paragraph>
+            <br /><br />
+            <Paragraph size='1' css={{ lineHeight: 'normal', fontFamily: '$pragmaticaExtended', fontWeight: 'normal' }}>
+              lets fucking move together.
+            </Paragraph>
+
+
+            <br /><br />
+            <Paragraph size='1' css={{ lineHeight: 'normal', fontFamily: '$pragmaticaExtended', fontWeight: 'normal' }}>
+              i love you like i love you, always.
+            </Paragraph>
+
+
+          </Container>
+          <Box
+            css={{
+              width: '100%',
+              color: '$mauve9',
+              margin: 'auto',
+              display: 'flex',
+              justifyContent: 'left',
+              paddingBottom: '10px',
+              paddingLeft: '25px',
+            }}
+          >
+            <ChvndlerCh width='200' />
+          </Box>
+        </Section>
+      </Box>
+    </>
+  );
+};
+
+
+export default Nash;
