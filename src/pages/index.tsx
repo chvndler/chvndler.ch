29274e010dda3732951f04f170b96b559123715f
chvndler <chan@ady.world>
2022-10-28 18:11:23 -0400

    Update Index Forward

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,13 @@
-import { Container, Text } from '@/components/ds';
-import { AppFooter } from '@/components/layout/app-footer';
-import { PageLayout } from '@/components/layout/page';
-import { AboutMe } from '@/components/sections/about-me';
-import { ProjectScroller } from '@/components/sections/project-scroller';
+import { useRouter } from 'next/dist/client/router';
+import { useEffect } from 'react';
 
 const Index = () => {
-  return (
-    <>
-      <PageLayout>
-        <Container size="1" css={{ paddingTop: 40 }}>
-          <Text
-            css={{
-              color: '$lime10',
-              fontFamily: '$druk',
-              fontSize: 24,
-              fontWeight: 'bold',
-              letterSpacing: 'normal',
-              textAlign: 'center'
-            }}
-          >
-            I'M CHAN.
-          </Text>
-          <Text
-            css={{
-              color: '$sage9',
-              lineHeight: '1',
-              fontFamily: '$subtle',
-              textTransform: 'uppercase',
-              fontSize: 12,
-              fontWeight: 'normal',
-              textAlign: 'center',
-              letterSpacing: '0.03em'
-            }}
-          >
-            Front-end Developer & Designer.
-          </Text>
-        </Container>
+  const router = useRouter();
+  useEffect(() => {
+    router.replace('/app');
+  });
 
-        <AboutMe />
-
-        <ProjectScroller />
-      </PageLayout>
-      <AppFooter />
-    </>
-  );
+  return;
 };
 
 export default Index;
