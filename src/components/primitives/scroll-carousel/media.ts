9b4fb445f8dc7174a1fbbc954449bd2538cd7e3d
chvndler <chan@ady.world>
2022-11-14 16:49:12 -0500

    up

--- src/components/primitives/scroll-carousel/media.ts
+++
@@ -1,5 +0,0 @@
-import media2 from '@/public/images/cover.jpg';
-import media1 from '@/public/og.png';
-
-export const media = [media1, media2];
-export const mediaByIndex = (index: number) => media[index % media.length];
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,29 +1,12 @@
 import { Meta } from '@/components/common/meta';
-import { Section } from '@/components/ds';
 import { PageLayout } from '@/components/layout/page';
-import { AtlrDsLogo } from '@/components/logos';
-import { ProjectDisplay } from '@/components/primitives/project-display';
-import { GridStyle } from '@/components/primitives/project-display/grid-style';
+
 // RENDER APP©...
 const TestPage = () => {
   return (
     <>
       <PageLayout>
         <Meta />
-
-        <Section size={'4'}>
-          <GridStyle>
-            <ProjectDisplay projectUrl="https://" title="Project Title" description="Project Description">
-              <AtlrDsLogo />
-            </ProjectDisplay>
-            <ProjectDisplay projectUrl="https://" title="Project Title" description="Project Description">
-              <AtlrDsLogo />
-            </ProjectDisplay>
-            <ProjectDisplay projectUrl="https://" title="Project Title" description="Project Description">
-              <AtlrDsLogo />
-            </ProjectDisplay>
-          </GridStyle>
-        </Section>
       </PageLayout>
     </>
   );
