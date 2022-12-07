a4bc26c927c2a5046d8f329a372c3f019431f72d
chvndler <chvndler.ch@icloud.com>
2022-12-07 02:11:59 -0500

    iuyvbl

---
+++ src/components/mirror/index.ts
@@ -0,0 +1,1 @@
+export { MirrorFooter } from './MirrorFooter';
--- src/hooks/use-ens-domain.ts
+++ src/hooks/use-ens-domain.ts
@@ -1,5 +1,3 @@
-import { contributorAddresses } from '@/data/ens';
-
-export const getConfig = () => {
-  return { ensDomain: process.env.NEXT_PUBLIC_AUTHOR_ENS, publicationAddress: contributorAddresses[0] };
+export const useEnsDomain = () => {
+  return { ensDomain: process.env.NEXT_PUBLIC_AUTHOR_ENS };
 };
