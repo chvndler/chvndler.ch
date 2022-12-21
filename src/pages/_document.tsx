fbffd5156e9db8eb79a25947644154180f9efc23
chvndler <chvndler.ch@icloud.com>
2022-12-21 04:33:29 -0500

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,7 @@
 /** @format */
 
 import Document, {DocumentContext, Head, Html, Main, NextScript} from 'next/document'
+import Script from 'next/script'
 import {getCssText, reset} from 'stitches.config'
 
 class App extends Document {
@@ -12,6 +13,7 @@ class App extends Document {
   return (
    <Html lang={'en'}>
     <Head>
+     <Script src='https://api.chvndler.ch/script.js' data-site='JZNGQCWH' defer></Script>
      <style id={'stitches'} dangerouslySetInnerHTML={{__html: getCssAndReset()}} />
     </Head>
     <body style={{opacity: '0'}}>
