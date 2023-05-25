d752cffe68e92c3b8e004fa862fbcac8a1a8ac2e
chvndler <chvndler.ch@icloud.com>
2023-05-25 05:34:48 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -25,12 +25,12 @@ const nextConfig = {
 
 const ContentSecurityPolicy = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' *.chvndler.ch api.chvndler.ch *.spotify.com *.plausible.io stream.mux cdn.usefathom.com *.usefathom.com *.mux.com;
-    child-src *.chvndler.ch api.chvndler.ch *.youtube.com *.google.com *.twitter.com stream.mux image.mux.com cdn.usefathom.com *.usefathom.com;
-    style-src 'self' 'unsafe-inline' *.chvndler.ch api.chvndler.ch *.mux.com;
-    img-src * blob: data: *.chvndler.ch api.chvndler.ch imagedelivery.net chvn.me image.mux.com;
-    media-src data: *.chvndler.ch api.chvndler.ch *.scdn.co stream.mux imagedelivery.net chvn.me image.mux.com *.mux.com cdn.usefathom.com *.usefathom.com;
-    connect-src data: * *.chvndler.ch api.chvndler.ch stream.mux market-assets.fra1.cdn.digitaloceanspaces.com imagedelivery.net image.mux.com *.mux.com cdn.usefathom.com *.usefathom.com;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' *.chvndler.ch api.chvndler.ch *.spotify.com vitals.vercel-insights.com stream.mux cdn.usefathom.com *.usefathom.com *.mux.com;
+    child-src *.chvndler.ch api.chvndler.ch *.youtube.com *.google.com vitals.vercel-insights.com *.twitter.com stream.mux image.mux.com cdn.usefathom.com *.usefathom.com;
+    style-src 'self' 'unsafe-inline' *.chvndler.ch api.chvndler.ch *.mux.com vitals.vercel-insights.com;
+    img-src * blob: data: *.chvndler.ch api.chvndler.ch imagedelivery.net chvn.me image.mux.com vitals.vercel-insights.com;
+    media-src data: *.chvndler.ch api.chvndler.ch *.scdn.co stream.mux imagedelivery.net chvn.me image.mux.com *.mux.com cdn.usefathom.com *.usefathom.com vitals.vercel-insights.com;
+    connect-src data: * *.chvndler.ch api.chvndler.ch vitals.vercel-insights.com *.usefathom.com;
     font-src 'self';
 `
 
