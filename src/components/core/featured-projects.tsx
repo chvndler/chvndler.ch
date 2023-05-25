bd89fed90ac3ef158340dd95e0f3b9e5e9923d49
chvndler <chvndler.ch@icloud.com>
2023-05-25 06:53:45 -0400

    mn'p

--- src/components/core/featured-projects.tsx
+++ src/components/core/featured-projects.tsx
@@ -5,8 +5,8 @@ import {BadgeDefault} from '../ui/badge'
 import {compareDesc} from 'date-fns'
 import {formatDateTime} from '../hooks/use-date-time'
 // ..
+import {siteURL} from '../../lib/const'
 import type {Projects} from 'contentlayer/generated'
-import {features} from 'process'
 type FeaturedProjectsProps = Projects
 
 /**
@@ -26,6 +26,47 @@ type FeaturedProjectsProps = Projects
  *       and to prevent us having to pass every `Projects` property.
  */
 
+function getImageUrl(featured: Projects) {
+  return `${siteURL}/` + featured.image
+}
+/*
+export default function List() {
+  const featured = allProjects
+  const listItems = featured.map((src) => (
+    <li
+      key={src._id}
+      className='flex flex-row items-center justify-between w-full h-auto py-3 mx-auto transition-all ease-out duration-400 gap-y-2 bg-blackA1 hover:bg-blackA2 hover:ease-in dark:bg-whiteA1 dark:hover:bg-whiteA3'
+    >
+      <>
+        <div className='flex flex-row items-center justify-between w-full'>
+          <div key={src._id} className='flex items-center justify-start'>
+            <p className='text-md font-fraktion font-[620] uppercase leading-5 tracking-normal text-grayscale-700'>
+              <Link href={src.url} target='_self' className=''>
+                {src.title}
+              </Link>
+            </p>
+          </div>
+
+          <div
+            key={src._id}
+            className='items-center justify-end hidden w-auto mx-auto sm:flex sm:flex-row sm:items-center'
+          >
+            <p className='text-right font-fraktion text-[11px] font-[580] uppercase leading-4 text-grayscale-700 dark:text-grayscale-800'>
+              <FormattedDate key={src._id} source={src} />
+            </p>
+          </div>
+        </div>
+      </>
+    </li>
+  ))
+  return (
+    <ul role='list' className='w-full divide-y divide-blackA5 dark:divide-whiteA5'>
+      {listItems}
+    </ul>
+  )
+}
+  */
+
 export const FeaturedProjects = () => {
   const featured = allProjects
   featured.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
@@ -42,33 +83,36 @@ export const FeaturedProjects = () => {
 
         <ul role='list' className='w-full divide-y divide-blackA5 dark:divide-whiteA5'>
           {featured.map((source) => (
-            <li
-              key={source._id}
-              className='duration-400 flex h-auto flex-row items-baseline justify-between gap-y-2 bg-blackA1 py-3 transition-all ease-out hover:bg-blackA2 hover:ease-in dark:bg-whiteA1 dark:hover:bg-whiteA3'
-            >
-              <div className='flex items-center'>
-                <p className='text-md font-fraktion font-[620] uppercase leading-5 tracking-normal text-grayscale-700'>
-                  <Link href={source.url} target='_self' className='w-full'>
-                    {source.title}
-                  </Link>
-                </p>
-              </div>
+            <>
+              <article key={source._id}>
+                <li
+                  key={source._id}
+                  className='flex flex-row items-baseline justify-between h-auto py-3 transition-all ease-out duration-400 gap-y-2 bg-blackA1 hover:bg-blackA2 hover:ease-in dark:bg-whiteA1 dark:hover:bg-whiteA3'
+                >
+                  <div key={source._id} className='flex items-center'>
+                    <p className='text-md font-fraktion font-[620] uppercase leading-5 tracking-normal text-grayscale-700'>
+                      <Link href={source.url} target='_self' className='w-full'>
+                        {source.title}
+                      </Link>
+                    </p>
+                  </div>
 
-              <div className='hidden gap-x-3 sm:flex sm:flex-row sm:items-end'>
-                <span className='mt-1 uppercase leading-4'>
-                  {source.tags.map((tag, index) => (
-                    <>
-                      <BadgeDefault>
-                        {source.tags.length - 1 === index ? tag : `${tag}`}
-                      </BadgeDefault>
-                    </>
-                  ))}
-                </span>
-                <p className='font-fraktion text-[11px] font-[580] uppercase leading-4 text-grayscale-700 dark:text-grayscale-800'>
-                  <FormattedDate key={source._id} source={source} />
-                </p>
-              </div>
-            </li>
+                  <div key={source._id} className='hidden gap-x-1 sm:flex sm:flex-row sm:items-end'>
+                    {source.tags.map((tag, index) => (
+                      <span key={source._id} className='leading-4 uppercase'>
+                        <BadgeDefault key={source._id}>
+                          {source.tags.length - 1 === index ? tag : `${tag}`}
+                        </BadgeDefault>
+                      </span>
+                    ))}
+
+                    <p className='font-fraktion text-[11px] font-[580] uppercase leading-4 text-grayscale-700 dark:text-grayscale-800'>
+                      <FormattedDate key={source._id} source={source} />
+                    </p>
+                  </div>
+                </li>
+              </article>
+            </>
           ))}
         </ul>
       </PageSection>
--- src/components/ui/git-sha.tsx
+++ src/components/ui/git-sha.tsx
@@ -1,33 +1,55 @@
+'use client'
+
+import {cn} from '../../lib/utils'
 import React from 'react'
 import Link from 'next/link'
 import {GitShaIcon} from '../svg/git'
 
-// server
+// client
 
-const isCommit = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA
-const isOwner = process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_OWNER
-const isSlug = process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG
+function useGitSha() {
+  const isCommit = '5dd284fa77c34328ce76ffeebe0022bd7a96ff08'
+  const isOwner = 'chvndler'
+  const isSlug = 'chvndler.ch'
+
+  const [sha, setSha] = React.useState('')
+  const [owner, setOwner] = React.useState('')
+  const [slug, setSlug] = React.useState('')
+
+  React.useEffect(() => {
+    setSha(isCommit)
+    setOwner(isOwner)
+    setSlug(isSlug)
+  }, [])
+
+  console.log('sha', sha, 'owner', owner, 'slug', slug)
+  return {sha, owner, slug}
+}
 
 export const CommitSha = () => {
-  const sha = isCommit
-  const owner = isOwner
-  const slug = isSlug
+  const {sha, owner, slug} = useGitSha()
+  const SHA = sha
+  const OWNER = owner
+  const GIT_SLUG = slug
 
   const color = 'currentColor'
 
-  if (sha && owner && slug) {
-    const shortSha = sha.substring(0, 6)
-    const commit_link = 'https://github.com/' + owner + '/' + slug + '/tree/' + sha
+  if (SHA && OWNER && GIT_SLUG) {
+    const shortSha = SHA.substring(0, 6)
+    const commit_link = 'https://github.com/' + OWNER + '/' + GIT_SLUG + '/tree/' + SHA
 
     return (
       <>
         <div className='flex flex-row items-center justify-center gap-x-2'>
           <span className='text-blackA9 dark:text-whiteA7'>SHA</span>
-          <span className='text-blackA9 dark:text-whiteA7 w-[13px] h-[13px]'>
+          <span className='h-[13px] w-[13px] text-blackA9 dark:text-whiteA7'>
             <GitShaIcon />
           </span>
           <Link href={commit_link} target='_blank' rel='noreferrer noopener'>
-            <span style={{color: `#${shortSha} !important`}}>{shortSha}</span>
+            <span className={cn(`text-[#${shortSha}]`)} style={{color: `#${shortSha} !important`}}>
+              {shortSha}
+            </span>
+            {/* <!-- <span className={cn('text-blackA9 dark:text-whiteA7', 'hover:underline')}>@{GIT_SLUG}</span> --> */}
           </Link>
         </div>
       </>
