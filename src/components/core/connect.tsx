5cbe905c340532db861b70e2bf8a38424b6a207d
chvndler <chvndler.ch@icloud.com>
2023-05-30 03:47:19 -0400

    update css

--- src/components/core/connect.tsx
+++ src/components/core/connect.tsx
@@ -5,9 +5,9 @@ import Tooltip from '../shared/tooltip';
 
 export const Connect = () => (
   <section id='connect-sect' className='py-6'>
-    <div className='flex flex-wrap justify-start flex-grow py-6 text-left gap-y-6 md:mt-0'>
+    <div className='flex flex-grow flex-wrap justify-start gap-y-6 py-6 text-left md:mt-0'>
       <div className='w-full space-y-4 md:w-1/2'>
-        <p className='text-cdsbx-600 mb-2 text-[18px] font-semibold'>Connect.</p>
+        <p className='mb-2 text-[18px] font-semibold text-grey-600'>Connect.</p>
         <nav className='mb-10 list-none text-grayA10 underline-offset-4'>
           {connects.map((connect) => (
             <>
@@ -18,7 +18,7 @@ export const Connect = () => (
                       key={connect.key}
                       href={connect.url}
                       target={connect.target}
-                      className='font-semibold lowercase text-md font-archivo text-grey-400 hover:text-grey-500 dark:text-grey-500 dark:hover:text-grey-600 md:text-sm'
+                      className='text-md font-archivo font-semibold lowercase text-grey-400 hover:text-grey-500 dark:text-grey-500 dark:hover:text-grey-600 md:text-sm'
                     >
                       {connect.title}
                     </Link>
@@ -31,7 +31,7 @@ export const Connect = () => (
       </div>
 
       <div className='w-full space-y-4 md:w-1/2'>
-        <p className='text-cdsbx-600 mb-2 text-[18px] font-semibold'>Other.</p>
+        <p className='mb-2 text-[18px] font-semibold text-grey-600'>Other.</p>
         <nav className='mb-10 list-none text-grayA10 underline-offset-4'>
           {profile.map((source) => (
             <>
@@ -39,7 +39,7 @@ export const Connect = () => (
                 <Link
                   href={source.url}
                   target={source.target}
-                  className='font-semibold lowercase text-md font-archivo text-grey-400 hover:text-grey-500 dark:text-grey-500 dark:hover:text-grey-600 md:text-sm'
+                  className='text-md font-archivo font-semibold lowercase text-grey-400 hover:text-grey-500 dark:text-grey-500 dark:hover:text-grey-600 md:text-sm'
                 >
                   {source.title}
                 </Link>
--- src/components/layout/featured.tsx
+++ src/components/layout/featured.tsx
@@ -15,12 +15,12 @@ export const FeaturedList = ({ projects }: ListProps) => {
   const listItems = sortedProjects.map((project) => (
     <li
       key={project._id}
-      className='transition-colors duration-300 ease-in-out bg-blackA1 hover:bg-blackA4 dark:bg-whiteA1 dark:hover:bg-whiteA4'
+      className='bg-blackA1 transition-colors duration-300 ease-in-out hover:bg-blackA4 dark:bg-whiteA1 dark:hover:bg-whiteA4'
     >
-      <div className='flex flex-row items-center justify-between w-full m-auto'>
+      <div className='m-auto flex w-full flex-row items-center justify-between'>
         <Link
           href={project.url}
-          className='items-center justify-start py-2 font-semibold tracking-tight text-md font-archivo text-grey-500 hover:text-grey-700 dark:hover:text-grey-300 md:text-lg'
+          className='text-md items-center justify-start py-2 font-archivo font-semibold tracking-tight text-grey-500 hover:text-grey-700 dark:hover:text-grey-300 md:text-lg'
         >
           {project.title}
         </Link>
@@ -43,9 +43,9 @@ export const FeaturedList = ({ projects }: ListProps) => {
 
   return (
     <section id='proj-sect' className='py-6'>
-      <div className='w-full py-6 mx-auto overflow-hidden'>
-        <p className='mb-2 font-semibold text-18 text-cdsbx-600'>Projects.</p>
-        <ul className='grid items-center grid-cols-1 divide-y divide-blackA6 dark:divide-whiteA4'>
+      <div className='mx-auto w-full overflow-hidden py-6'>
+        <p className='mb-2 text-[18px] font-semibold text-grey-600'>Projects.</p>
+        <ul className='grid grid-cols-1 items-center divide-y divide-blackA6 dark:divide-whiteA4'>
           {listItems}
         </ul>
       </div>
