dd16cfae9815ed910868fb60d10d6a6f69a4caad
chvndler <chvndler.ch@icloud.com>
2023-06-27 09:52:09 -0400

    Update highlights.tsx

--- src/components/core/projects/highlights.tsx
+++ src/components/core/projects/highlights.tsx
@@ -15,25 +15,25 @@ export const ProjectHighlights = ({ projects }: ListProps) => {
 
   const list = sortedProjects.map((prxjxct, z) => (
     <li key={z}>
-      <div className='grid items-start grid-cols-1 gap-6 text-carbon-500 md:grid-cols-3'>
-        <div className=''>
-          <FormattedDate postDate={prxjxct} />
-        </div>
-
-        <div className='w-full gap-y-2 md:col-span-2'>
-          <p className='text-[14px] font-[600] text-carbon-600 dark:text-carbon-400'>
-            <Link href={prxjxct.slug} className='tracking-normal'>
-              {prxjxct.title}
-            </Link>
-          </p>
+      <div key={z} className='grid gap-6 mt-3'>
+        <div className='grid items-start grid-cols-1 text-carbon-500 md:grid-cols-3'>
           <div>
-            {prxjxct.tags.map((tag, p) => (
-              <TagChip
-                key={p}
-                className='hidden border border-slate-400 bg-blackA3 font-sohne text-[8.5px] font-[500] uppercase text-blackA9 dark:border-slate-600 dark:bg-whiteA2 dark:text-whiteA8 sm:inline'>
-                {tag}
-              </TagChip>
-            ))}
+            <FormattedDate postDate={prxjxct} />
+          </div>
+
+          <div className='w-full md:col-span-2'>
+            <p className='font-mono text-xs font-semibold text-carbon-500 dark:text-carbon-600'>
+              <Link href={prxjxct.slug} className='tracking-normal'>
+                {prxjxct.title}
+              </Link>
+            </p>
+            <p className='text-[14px] font-[400] text-carbon-400 dark:text-carbon-400'>
+              {prxjxct.tags.map((tag, i) => (
+                <span key={i}>
+                  <Link href={`/tags/${tag}`}>{tag}</Link>
+                </span>
+              ))}
+            </p>
           </div>
         </div>
       </div>
@@ -41,22 +41,20 @@ export const ProjectHighlights = ({ projects }: ListProps) => {
   ));
 
   return (
-    <section id='project-list' className=''>
-      <div className='w-full py-6 mx-auto overflow-hidden'>
-        <div className='grid gap-6 mt-3'>
-          <p className='pb-4 text-[16px] font-[500] leading-6 text-carbon-700 dark:text-carbon-50'>
-            Projects
-          </p>
-          <ul className=''>{list}</ul>
-        </div>
+    <section>
+      <div className='text-sm font-sohne'>
+        <p className='pb-4 text-[16px] font-[500] leading-6 text-carbon-700 dark:text-carbon-50'>
+          Projects
+        </p>
+        <ul className=''>{list}</ul>
+      </div>
 
-        <div className='flex justify-start'>
-          <Link
-            href='/projects'
-            className='text-left font-sohne text-sm font-[500] text-carbon-500 hover:text-carbon-700 dark:hover:text-carbon-300'>
-            view all projects →
-          </Link>
-        </div>
+      <div className='flex justify-start'>
+        <Link
+          href='/projects'
+          className='text-left font-sohne text-sm font-[500] text-carbon-500 hover:text-carbon-700 dark:hover:text-carbon-300'>
+          view all projects →
+        </Link>
       </div>
     </section>
   );
