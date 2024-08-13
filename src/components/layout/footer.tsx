ac54aa6eb83022d60301fa09dbb1f22b6e285503
chvndler <chvndler.ch@icloud.com>
2024-08-13 12:03:52 -0400

    Update footer.tsx

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -6,8 +6,7 @@ export const Footer = () => {
     <footer className='relative w-full border-t border-carbon-100 px-3 dark:border-carbon-800'>
       <div className='mx-auto my-2 flex max-w-[620px] items-center justify-center py-2 text-center'>
         <span className='text-left font-sohne text-[10px] font-[400] uppercase leading-4 tracking-wide text-carbon-500 dark:text-carbon-600'>
-          {/* <!-- hold <ModularTime /> in state --> */}© something like this
-          but not this
+          © something like this but not this
         </span>
 
         <div className='app-gray grow text-right font-sohne text-[13px] font-[500] hover:text-carbon-700 dark:text-carbon-600 dark:hover:text-carbon-600'>
